3cc7ad15617a2a992fd74be1ef2f81a798c64881
Andrej Krivulčík <andrej@krivulcik.com>
2022-11-24 06:29:48 +0100

    Initial commit

---
+++ MailgunTest/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MailgunTest;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(SendSimpleMessageChunk.SendSimpleMessage().Content.ToString());
---
+++ MailgunTest/SendSimpleMessageChunk.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+
+namespace MailgunTest
+{
+    public static class SendSimpleMessageChunk
+    {
+        public static RestResponse SendSimpleMessage()
+        {
+            RestClient client = new RestClient("https://api.mailgun.net/v3/");
+            client.Authenticator =
+                new HttpBasicAuthenticator("api",
+                                            "MY_API_KEY");
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain", "MY_DOMAIN", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "Excited User <mailgun@MY_DOMAIN>");
+            request.AddParameter("to", "MY_EMAIL");
+            request.AddParameter("subject", "Hello");
+            request.AddParameter("text", "Testing some Mailgun awesomness!");
+            request.Method = Method.Post;
+            return client.Execute(request);
+        }
+    }
+}
